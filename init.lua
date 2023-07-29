ccd64c38711cbe173cf5cfa5b152633fc12f4660
sunxin <sunxin.181@bytedance.com>
2023-07-29 17:46:21 +0800

    [feat] use more plugin

--- init.lua
+++ init.lua
@@ -12,4 +12,17 @@ require "user.bufferline"
 require "user.telescope"
 require "user.utils"
 require "user.diffview"
+require('neoscroll').setup()
+require'marks'.setup {
+  mappings = {
+    set_next = "m,",
+    next = "m]",
+    preview = "m:",
+    set_bookmark0 = "m0",
+    prev = false -- pass false to disable only this default mapping
+  }
+}
+require("fidget").setup {
+  -- options
+}
 vim.cmd[[colorscheme tokyonight]]
